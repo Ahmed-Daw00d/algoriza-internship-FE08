950aaae3159c10c444e3768b9aecbf3b4f3ff726
Ahmed-Daw00d <adawood331@gmail.com>
2023-11-30 18:10:03 +0200

    1

---
+++ sign-screen/components/sign/sign.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign',
+  templateUrl: './sign.component.html',
+  styleUrl: './sign.component.scss'
+})
+export class SignComponent {
+title:string="Sign in"
+reTitle:string="Register"
+word:string="Don't"
+
+}
---
+++ sign-screen/sign-screen.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignComponent } from './components/sign/sign.component';
+
+
+
+@NgModule({
+  declarations: [
+    SignComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[]
+})
+export class SignScreenModule { }
