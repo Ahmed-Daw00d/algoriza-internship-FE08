1eb2167e66a014462a494156938fa33022b0d784
Ahmed-Daw00d <adawood331@gmail.com>
2023-12-02 14:33:57 +0200

    1

--- sign-screen/components/sign/sign.component.ts
+++ sign-screen/components/sign/sign.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-sign',
-  templateUrl: './sign.component.html',
-  styleUrl: './sign.component.scss'
+  selector: "app-sign",
+  templateUrl: "./sign.component.html",
+  styleUrl: "./sign.component.scss",
 })
 export class SignComponent {
-title:string="Sign in"
-reTitle:string="Register"
-word:string="Don't"
+  title: string = "Sign in";
+  reTitle: string = "Register";
+  word: string = "Don't";
+  //
+  form!: FormGroup;
+  user: any[] = [];
+  alert: boolean = false;
+  //
+  constructor(private build: FormBuilder) {}
+  ngOnInit() {
+    this.form = this.build.group({
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+      confirm: [""],
+    });
+  }
 
+  //
+  convert() {
+    this.title == "Sign in"
+      ? (this.title = "Register")
+      : (this.title = "Sign in");
+    this.reTitle == "Register"
+      ? (this.reTitle = "Sign in")
+      : (this.reTitle = "Register");
+      this.alert=false;
+  }
+  //
+  register() {
+    if (this.title == "Register") {
+      if ("user" in localStorage) {
+        this.user = JSON.parse(localStorage.getItem("user")!);
+        this.user.push(this.form.value);
+        localStorage.setItem("user", JSON.stringify(this.user));
+      } else {
+        this.user.push(this.form.value);
+        localStorage.setItem("user", JSON.stringify(this.user));
+      }
+
+    } else {
+      this.signIn();
+    }
+    
+  }
+  //
+  signIn() {
+    if ("user" in localStorage) {
+      this.user = JSON.parse(localStorage.getItem("user")!);
+      let x = this.user.find(
+        (res) =>
+          res.email == this.form.value.email &&
+          res.password == this.form.value.password
+      );
+      x !== undefined ? (this.alert = false) : (this.alert = true);
+  console.log(x)
+    } else {
+      this.convert();
+    }
+  }
 }
--- sign-screen/sign-screen.module.ts
+++ sign-screen/sign-screen.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SignComponent } from './components/sign/sign.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SignComponent } from "./components/sign/sign.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-  declarations: [
-    SignComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports:[]
+  declarations: [SignComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule,BrowserModule],
+  exports: [],
 })
-export class SignScreenModule { }
+export class SignScreenModule {}
