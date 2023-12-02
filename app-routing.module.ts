7f9cde24461039eab7c29e1e477cf6894b8116e5
Ahmed-Daw00d <adawood331@gmail.com>
2023-12-02 22:14:59 +0200

    1

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignComponent } from './sign-screen/components/sign/sign.component';
+import { HomeComponent } from './home/components/home/home.component';
 
 const routes: Routes = [
   {path:"sign", component:SignComponent},
+  {path:"home", component:HomeComponent},
+  {path:"**",redirectTo:"home",pathMatch:"full"}
 ];
 
 @NgModule({
